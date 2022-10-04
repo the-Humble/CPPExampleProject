dd9374809ac7329093cdbf1d35b25533cc0d1ed5
Jose Ignacio Ferrer Vera <joseferrertec@gmail.com>
2022-10-04 00:37:19 -0500

    Speed tutorials

--- src/gdexample.cpp
+++ src/gdexample.cpp
@@ -4,6 +4,8 @@ using namespace godot;
 
 void GDExample::_register_methods() {
     register_method("_process", &GDExample::_process);
+    register_property<GDExample, float>("amplitude", &GDExample::amplitude, 10.0);
+    register_property<GDExample, float>("speed", &GDExample::set_speed, &GDExample::get_speed, 1.0);
 }
 
 GDExample::GDExample() {
@@ -13,15 +15,31 @@ GDExample::~GDExample() {
     // add your cleanup here
 }
 
-void GDExample::_init() {
+void GDExample::_init()
+{
     // initialize any variables here
     time_passed = 0.0;
+    amplitude = 10.0;
+    speed = 1.0;
 }
 
-void GDExample::_process(float delta) {
-    time_passed += delta;
+void GDExample::_process(float delta)
+{
+    time_passed += speed * delta;
 
-    Vector2 new_position = Vector2(10.0 + (10.0 * sin(time_passed * 2.0)), 10.0 + (10.0 * cos(time_passed * 1.5)));
+    Vector2 new_position = Vector2(
+        amplitude + (amplitude * (float)sin(time_passed * 2.0)),
+        amplitude + (amplitude * (float)cos(time_passed * 1.5)));
 
     set_position(new_position);
+}
+
+void GDExample::set_speed(float p_speed)
+{
+    speed = p_speed;
+}
+
+float GDExample::get_speed()
+{
+    return speed;
 }
